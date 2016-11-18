2a5627993f5482f7a4cf77cd829e48a416962729
Lord Niels N. Horn <i.am@nilq.dk>
2016-11-19 00:55:44 +0100

    Add basic boot_state

---
+++ static/js/states/boot.js
@@ -0,0 +1,18 @@
+var Platformer = Platformer || {};
+
+Platformer.boot_state = {
+  preload: function() {
+    this.load.image("load-bar", "assets/img/load-bar.png");
+    this.load.bitmapFont("carrier_command", "assets/font/carrier_command.png", "assets/font/carrier_command.xml");
+  },
+
+  create: function() {
+    this.game.stage.backgroundColor = "#02171f";
+
+    this.scale.scaleMode             = Phaser.ScaleManager.SHOW_ALL;
+    this.scale.pageAlignHorizontally = true;
+    this.scale.pageAlignVertically   = true;
+
+    this.game.physics.startSystem(Phaser.Physics.ARCADE);
+  }
+};
